18f14fe68df080b1521f92fcfde5b208ce09d3e3
MohammedDuke <hamoahmed8055@gmail.com>
2023-12-01 14:24:03 -0800

    fix error

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,14 +2,16 @@
 """
 Python script that takes 2 arguments in order to solve the given challenge
 """
-import requests
-from sys import argv
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
 
-if __name__ == '__main__':
-    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
-    r = requests.get(url)
-    commits = r.json()
-    for commit in commits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
-
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
+    commits = get(url).json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
