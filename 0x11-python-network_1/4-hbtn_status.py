c5aa74fad5254f9b52f5f0d1050de212bb5668e3
MohammedDuke <hamoahmed8055@gmail.com>
2023-12-01 14:21:29 -0800

    fix errors

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,13 +2,11 @@
 """
 Script that fetches https://intranet.hbtn.io/status
 """
-import requests
 
-if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
-    print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+if __name__ == "__main__":
+    from requests import get
 
+    html = get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
